c671f158da29e8b2bb2639fe968f659cc1e526fe
Kalyan <kalyanmakedevopseasy@gmail.com>
2026-06-27 23:12:26 +0530

    Fix terraform fmt alignment in keyvault and provider files

--- challenge-1/providers.tf
+++ challenge-1/providers.tf
@@ -15,8 +15,8 @@ provider "azurerm" {
   features {
     key_vault {
       purge_soft_deleted_keys_on_destroy         = var.kv_purge_on_destroy
-      purge_soft_deleted_secrets_on_destroy       = var.kv_purge_on_destroy
-      purge_soft_deleted_certificates_on_destroy  = var.kv_purge_on_destroy
+      purge_soft_deleted_secrets_on_destroy      = var.kv_purge_on_destroy
+      purge_soft_deleted_certificates_on_destroy = var.kv_purge_on_destroy
     }
   }
   subscription_id = var.subscription_id
