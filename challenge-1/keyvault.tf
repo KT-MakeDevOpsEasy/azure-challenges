16139a295dafe1f4b0a3012001a471574149ac19
Kalyan Teja <kalyanmakedevopseasy@gmail.com>
2026-06-28 10:55:23 +0530

    Dev (#13)
    * Add workflow self-paths to trigger on pipeline changes
    
    * Add environment comparison table to challenge-1 README (#10)
    
    * Feature/prod deployment (#12)
    
    * Add environment comparison table to challenge-1 README
    
    * Fix prod: unique KV name with app ID, use Standard_D2as_v4 for westeurope

--- challenge-1/keyvault.tf
+++ challenge-1/keyvault.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv" {
   count = var.enable_keyvault ? 1 : 0
 
-  name                       = "kv-${local.name_suffix}"
+  name                       = "kv-${local.name_suffix}-${var.application_id}"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
   tenant_id                  = data.azurerm_client_config.current.tenant_id
